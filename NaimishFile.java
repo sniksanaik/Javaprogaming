a441720d1ba418a0075e92da52e8ab3edcc6db04
sniksa <sniksa10@gmail.com>
2026-06-16 21:05:09 +0530

    new commit on 16-06-26

--- NaimishFile.java
+++ NaimishFile.java
@@ -1,5 +1,10 @@
 class NaimishFile{
     public static void main(String[] args) {
 
+        System.out.println("sfdkjbgksjdn");
+        System.out.println("sfdkjbgksjdn");
+        System.out.println("sfdkjbgksjdn");
+        System.out.println("sfdkjbgksjdn");
+
     }
 }
