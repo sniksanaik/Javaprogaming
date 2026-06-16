8c3adf4ab90bce711153a4a3f51ca063feb5258c
naimish <patel.naimishkumar@gmail.com>
2026-06-16 22:48:22 +0530

    conflict

--- NaimishFile.java
+++ NaimishFile.java
@@ -1,11 +1,12 @@
 class NaimishFile{
     public static void main(String[] args) {
 
-        System.out.println("sfdkjbgksjdn");
-        System.out.println("sfdkjbgksjdn");
-        System.out.println("sfdkjbgksjdn");
-        System.out.println("sfdkjbgksjdn");
-        System.out.println("sniksa change 1");
+        System.out.println("conflit test ");
+        System.out.println("conflit test ");
+        System.out.println("conflit test ");
+        System.out.println("conflit test ");
+        System.out.println("conflit test ");
+        System.out.println("conflit test ");
 
         System.out.println("12222");
         System.out.println("wrwr");
